c99e239fb3b9b419d4e6f783c344c54a1edc8c57
sagar0211 <sagarparihar002@gmail.com>
2026-03-20 23:28:34 +0530

    Update card image styles and adjust image source for better display

--- src/movieCard.js
+++ src/movieCard.js
@@ -4,7 +4,7 @@ function MovieCard({movie}) {
     return(
             <div className="card" key={movie.id}>
                 <img className="card--image"
-                    src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/w342${movie.poster_path}`}
                     alt={movie.title + ' poster'}
                 />
                 <div className="card--content">
